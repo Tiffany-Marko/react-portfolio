b1c76e6c0c14aacf573b6b95fe7d45d3c23ffa75
Tiffany Marko <tiffanyjmarko@gmail.com>
2022-12-06 16:59:42 -0800

    up

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { validateEmail } from "../utils/helpers";
@@ -8,11 +8,15 @@ const divStyles = {
     margin: "10px auto",
 }
 function Contact() {
-    const [formState, setFormState]=useState({name: "", email: "", message: ""})
+    const [formState, setFormState] = useState({ name: "", email: "", message: "" })
     const handleInputChange = (e) => {
-   console.log(e.target.name, e.target.value)
-//    the above needs to be modified
-      };
+        console.log(e.target.name, e.target.value)
+        //    the above needs to be modified
+        setFormState({
+            ...formState,
+        [e.target.name]: e.target.value
+        })
+    };
     return (
         <div style={divStyles}>
             <h1>Contact</h1>
@@ -20,31 +24,31 @@ function Contact() {
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicName">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control 
-                    type="text" 
-                    placeholder="Enter Name"
-                    name="name"
-                    value={formState.name}
-                    onChange={handleInputChange} />
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter Name"
+                        name="name"
+                        value={formState.name}
+                        onChange={handleInputChange} />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control 
-                    type="email" 
-                    placeholder="Enter email" 
-                    name="email"
-                    value={formState.email}
-                    onChange={handleInputChange} />
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        name="email"
+                        value={formState.email}
+                        onChange={handleInputChange} />
 
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicMessage">
                     <Form.Label>Message</Form.Label>
-                    <Form.Control 
-                    as="textarea" 
-                    placeholder="Enter message" 
-                    value={formState.message}
-                    name="message"
-                    onChange={handleInputChange} />
+                    <Form.Control
+                        as="textarea"
+                        placeholder="Enter message"
+                        value={formState.message}
+                        name="message"
+                        onChange={handleInputChange} />
                 </Form.Group>
 
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,5 +1,15 @@
 import React from "react"
 function Portfolio(){
+    const projects = [
+        {
+            name:"",
+            github:"",
+            image:"",
+            link:"",
+            description:"",
+
+        }
+    ]
     return(
         <div><h1>Portfolio</h1></div>
     )
